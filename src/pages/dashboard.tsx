f940482b3d4289dea942b47d0b0323928ed3ac1f
amackzie@gmail.com <amackzie@gmail.com>
2025-03-09 17:22:41 +0300

    add some styling to the homepage

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,37 +6,43 @@ export const Dashboard: FC = () => {
   const [selected, setSelected] = useState<Date[] | undefined>();
   return (
     <>
-      <div className="p-10 flex flex-row gap-5 wrap">
-        <DayPicker
-          mode="multiple"
-          selected={selected}
-          onSelect={setSelected}
-        ></DayPicker>
-        <div className="card bg-base-100 image-full w-90 h-20 shadow-sm">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Daily Streak</h2>
-            <p>Keep working out, you're doing great</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Just do it</button>
+      <div className="p-10 flex flex-row gap-5">
+        <div>
+          <DayPicker
+            mode="multiple"
+            selected={selected}
+            onSelect={setSelected}
+          ></DayPicker>
+        </div>
+        <div className="flex flex-row gap-5">
+          <div className="card bg-base-100 image-full w-80 h-50 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Daily Streak</h2>
+              <p>Keep working out, you're doing great</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Just do it</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card bg-primary text-primary-content w-96">
-          <div className="card-body">
-            <h2 className="card-title">Steps walked</h2>
-            <p>You've taken 1000 steps</p>
-          </div>
-        </div>
-        <div className="card bg-secondary text-primary-content w-96">
-          <div className="card-body">
-            <h2 className="card-title">Weight Lost</h2>
-            <p>You've lost around 10kg </p>
+          <div className="flex flex-row wrap gap-3">
+            <div className="card bg-primary text-primary-content h-40 w-40 rounded-xl">
+              <div className="card-body">
+                <h2 className="card-title">Calories Burned 🔥</h2>
+                <p>2,457</p>
+              </div>
+            </div>
+            <div className="card bg-secondary text-primary-content h-40 w-40 rounded-xl">
+              <div className="card-body">
+                <h2 className="card-title">Steps Walked</h2>
+                <p>You've taken 5,689 steps or walked 10KM </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
