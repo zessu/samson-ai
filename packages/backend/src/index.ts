52cb6d010ed1ecaaa83f48f8f5c027676adf3f5e
amackzie@gmail.com <amackzie@gmail.com>
2025-05-04 14:34:10 +0300

    add drizzle config and supabase postgres db

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/generate", (c) => {
+  return c.text("generated your workout routine");
+});
+
+export default app;
