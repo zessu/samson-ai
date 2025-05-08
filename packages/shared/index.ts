3b409d98dcf20bc30e9d96c4f37f41dfacdc5ec9
amackzie@gmail.com <amackzie@gmail.com>
2025-05-08 08:41:17 +0300

    move schema to shared folder

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,5 +1,31 @@
-export type Human = {
-  name: string;
-  age: number;
-  address: string;
-};
+import { z } from "zod";
+
+export const onBoardingSchema = z.object({
+  gender: z.enum(["Male", "Female"]),
+  age: z.number(),
+  weight: z.number(),
+  fitnessLevel: z.enum(["beginner", "intermediate", "advanced"]),
+  goals: z.array(z.string()),
+  equipment: z.array(z.string()),
+  weekdays: z.array(
+    z.enum([
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ])
+  ),
+  time: z.string(),
+  offset: z.string(),
+  duration: z.number(),
+  notifications: z.object({
+    email: z.boolean(),
+    sms: z.boolean(),
+    app: z.boolean(),
+  }),
+});
+
+export type onBoardingState = z.infer<typeof onBoardingSchema>;
