efd3f86873c2e6912f990690d5f3ac149289479f
amackzie@gmail.com <amackzie@gmail.com>
2025-05-13 18:07:30 +0300

    restructure model prompt commands

--- packages/backend/lib/agent.ts
+++ packages/backend/lib/agent.ts
@@ -1,11 +1,6 @@
 const samson_ul = Bun.env.SAMSON_URL as string;
 import { routineType } from "@lib/index";
 
-const x = {
-  prompt: "",
-  data: "",
-};
-
 export const queryAgent = async (data: routineType) => {
   const promptInfo = {
     prompt: Bun.env.SAMSON_PROMPT as string,
@@ -13,7 +8,12 @@ export const queryAgent = async (data: routineType) => {
   };
 
   const payload = {
-    messages: [promptInfo],
+    messages: [
+      {
+        role: "user",
+        content: `${promptInfo.prompt} : User data is -> ${JSON.stringify(data)}`,
+      },
+    ],
   };
 
   return await fetch(samson_ul, {
