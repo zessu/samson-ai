9bf82c0ad9653d7173e92ad4804f1de7e8bc2930
amackzie@gmail.com <amackzie@gmail.com>
2025-05-15 21:22:45 +0300

    send unformatted email

--- packages/backend/lib/workers/mail.worker.ts
+++ packages/backend/lib/workers/mail.worker.ts
@@ -1,19 +1,38 @@
 import { Worker, Job } from "bullmq";
 import IORedis from "ioredis";
+import { Resend } from "resend";
+import { eq } from "drizzle-orm";
+
+import { db } from "@db/index";
+import { user } from "@/auth-schema";
 import { mailSend } from "@lib/index";
 
 const connection = new IORedis(`${Bun.env.REDIS_HOST}:${Bun.env.REDIS_PORT}`, {
   maxRetriesPerRequest: null,
 });
 
+const resend = new Resend(Bun.env.RESEND_API_KEY);
+
 export const mailWorker = () => {
   const mailWorker = new Worker(
     "sendMail",
     async (job: Job<mailSend>) => {
       const jobData = job.data;
-      console.log(
-        `workout ${jobData.workout} calories ${jobData.calories} caution ${jobData.caution} userid ${jobData.userId}`
-      );
+      const userid = job.data.userId;
+      const res = await db
+        .select({ email: user.email })
+        .from(user)
+        .where(eq(user.id, userid));
+
+      if (res.length === 0)
+        throw new Error("Could not find a user with that id");
+
+      resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: res[0].email,
+        subject: "Hello World",
+        html: `<p>Here here is your workout <br />workout ${jobData.workout} <br />calories ${jobData.calories} <br />caution ${jobData.caution}</p>`,
+      });
     },
     {
       connection,
