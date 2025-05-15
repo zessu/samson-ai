b6d70bb06c363e0ad25a57ed8311bd1b89480c48
amackzie@gmail.com <amackzie@gmail.com>
2025-05-15 17:15:15 +0300

    poll for workouts

--- packages/backend/lib/workers/workoutpoller.worker.ts
+++ packages/backend/lib/workers/workoutpoller.worker.ts
@@ -1,21 +1,49 @@
-import { z } from "zod";
 import { Worker, Job } from "bullmq";
+import { gte, lt, and, arrayOverlaps } from "drizzle-orm";
 import IORedis from "ioredis";
-import { nanoid } from "nanoid";
+const dayjs = require("dayjs");
 
-import { workoutScheduleInsertSchema, workoutSchedule } from "@db/schema/index";
+import { workoutSettings } from "@db/schema/index";
 import { db } from "@db/index";
-import { queryAgent } from "@lib/agent";
 
 const connection = new IORedis(`${Bun.env.REDIS_HOST}:${Bun.env.REDIS_PORT}`, {
   maxRetriesPerRequest: null,
 });
 
+const weekMap = {
+  0: "sunday",
+  1: "monday",
+  2: "tuesday",
+  3: "wednesday",
+  4: "thursday",
+  5: "friday",
+  6: "saturday",
+} as const;
+
 export const workoutMailWorker = () => {
   const workoutMailWorker = new Worker(
     "workoutMailQueue",
     async (job) => {
-      console.log("recevied a new job");
+      const dayofTheWeek = (dayjs().day() as keyof typeof weekMap) ?? 0;
+      const mappedDay = weekMap[dayofTheWeek];
+      const hour = dayjs().hour(); // e.g 16
+      const startTime = `${hour.toString().padStart(2, "0")}:00:00`;
+      const endTime = `${(hour + 1).toString().padStart(2, "0")}:00:00`;
+      const result = await db
+        .select()
+        .from(workoutSettings)
+        .where(
+          and(
+            gte(workoutSettings.workoutTime, startTime),
+            lt(workoutSettings.workoutTime, endTime),
+            arrayOverlaps(workoutSettings.weekdays, [mappedDay])
+          )
+        );
+
+      if (!result[0])
+        throw new Error("No users scheduled a workout at this time");
+
+      console.log(result);
     },
     { connection }
   );
