cdf133925d2ab16e70c1ae7b024fd92ef7a276e4
amackzie@gmail.com <amackzie@gmail.com>
2025-05-17 22:17:38 +0300

    remove unneeded file

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     tailwindcss(),
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
