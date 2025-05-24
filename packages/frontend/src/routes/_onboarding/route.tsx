04635f20568ec02542e523ff1f6000fc427f0fbe
amackzie@gmail.com <amackzie@gmail.com>
2025-05-24 16:26:41 +0300

    get correct session image

--- packages/frontend/src/routes/_onboarding/route.tsx
+++ packages/frontend/src/routes/_onboarding/route.tsx
@@ -1,22 +1,23 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
-import { getSession } from "../../lib/auth";
+import { getSession, useSession } from "../../lib/auth";
 
 export const Route = createFileRoute("/_onboarding")({
   beforeLoad: async () => {
     const { data: session } = await getSession();
-    console.log("session", session);
     if (!session) return redirect({ to: "/signup" });
   },
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const session = useSession();
+  const imgUrl = session.data?.user.image ?? "";
   return (
     <>
       <div className="flex flex-row justify-end p-2">
         <div className="avatar">
           <div className="w-8 rounded-full mr-5 cursor-pointer">
-            <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+            <img src={imgUrl} />
           </div>
         </div>
       </div>
