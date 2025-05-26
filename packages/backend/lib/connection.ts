2756146c6f26bba738312d964d9fa20f472c29b6
amackzie@gmail.com <amackzie@gmail.com>
2025-05-26 18:59:28 +0300

    remove native bun redis client - lack proper support  bullmq

--- packages/backend/lib/connection.ts
+++ packages/backend/lib/connection.ts
@@ -1,8 +1,10 @@
+import { RedisClient } from "bun";
 import IORedis from "ioredis";
 
 export const connection = new IORedis(
   `${Bun.env.REDIS_HOST}:${Bun.env.REDIS_PORT}`,
   {
-    maxRetriesPerRequest: null,
+    maxRetriesPerRequest: 3,
+    socketTimeout: 30000,
   }
 );
