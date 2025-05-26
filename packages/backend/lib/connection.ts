6aa46df60bc9e5af644cfaf17a2e706261c440f7
amackzie@gmail.com <amackzie@gmail.com>
2025-05-26 19:36:36 +0300

    change redis connection options

--- packages/backend/lib/connection.ts
+++ packages/backend/lib/connection.ts
@@ -1,10 +1,21 @@
-import { RedisClient } from "bun";
 import IORedis from "ioredis";
 
-export const connection = new IORedis(
-  `${Bun.env.REDIS_HOST}:${Bun.env.REDIS_PORT}`,
-  {
-    maxRetriesPerRequest: 3,
-    socketTimeout: 30000,
-  }
-);
+const connection = new IORedis(`${Bun.env.REDIS_HOST}:${Bun.env.REDIS_PORT}`, {
+  maxRetriesPerRequest: 3,
+  socketTimeout: 30000,
+});
+
+connection.on("connect", () => {
+  console.log("Client connected to Redis");
+});
+connection.on("error", (err) => {
+  console.error("Redis Redis error:", err);
+});
+connection.on("reconnecting", () => {
+  console.log("Reconnecting to Redis");
+});
+connection.on("end", () => {
+  console.log("Redis connection closed");
+});
+
+export { connection };
