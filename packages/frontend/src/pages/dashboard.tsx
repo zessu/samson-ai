4812b285285f95e2632c2c8f073908c66dd2dd31
amackzie@gmail.com <amackzie@gmail.com>
2025-05-26 12:14:08 +0300

    fix margin on dashboard - make all elements visible

--- packages/frontend/src/pages/dashboard.tsx
+++ packages/frontend/src/pages/dashboard.tsx
@@ -19,42 +19,42 @@ export const Dashboard: FC = () => {
 
   return (
     <>
-      {!generatedWorkout && !doneGenerating && (
-        <>
-          <div className="flex flex-row align-center items-center">
-            <div className="inline-grid *:[grid-area:1/1] mr-2">
-              <div className="status status-primary animate-ping"></div>
-              <div className="status status-primary"></div>
-            </div>{" "}
-            Configuring your profile and generating some workouts for you.
+      <div className="flex flex-col justify-center items-center w-full -mt-20">
+        {!generatedWorkout && !doneGenerating && (
+          <>
+            <div className="flex flex-row align-center items-center">
+              <div className="inline-grid *:[grid-area:1/1] mr-2">
+                <div className="status status-primary animate-ping"></div>
+                <div className="status status-primary"></div>
+              </div>{" "}
+              Configuring your profile and generating some workouts for you.
+            </div>
+          </>
+        )}
+        {doneGenerating && (
+          <div>
+            <div role="alert" className="alert alert-success mb-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>
+                Congratulations🥳🎉 You will get daily work-outs sent directly
+                to your email every day an hour before your next workout
+              </span>
+            </div>
           </div>
-        </>
-      )}
-      {doneGenerating && (
-        <div>
-          <div role="alert" className="alert alert-success mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>
-              Congratulations🥳🎉 You will get daily work-outs sent directly to
-              your email every day an hour before your next workout
-            </span>
-          </div>
-        </div>
-      )}
-      <div className="flex flex-col justify-center items-center w-full">
-        <img src="/wip.png" width="300" height="300" className="w-80 h-80" />
+        )}
+        <img src="/wip.png" width="250" height="250" className="w-80 h-80" />
         <p className="font-bold text-xs w-80 ">
           The dashboard is still a work in progress. You will still get emails
           and notifications in your mailbox an hour to your workout. I'll send
