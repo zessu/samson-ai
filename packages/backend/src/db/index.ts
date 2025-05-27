4cb33745b731bf250d1aa142be604c09c13172d2
amackzie@gmail.com <amackzie@gmail.com>
2025-05-27 15:33:06 +0300

    add prod urls

--- packages/backend/src/db/index.ts
+++ packages/backend/src/db/index.ts
@@ -1,4 +1,4 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(Bun.env.DATABASE_URL);
+export const db = drizzle(Bun.env.DATABASE_URL as string);
