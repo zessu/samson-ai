5a6f3fffe435ee06dab3d23cb5ecb167b58c1f05
amackzie@gmail.com <amackzie@gmail.com>
2025-06-02 19:27:07 +0300

    remove this code block
    did not work - seems to be a broader issue with render.com

--- packages/backend/src/db/index.ts
+++ packages/backend/src/db/index.ts
@@ -12,15 +12,3 @@ console.log(Bun.env.BETTER_AUTH_URL);
 console.log(">>>>>>>>>>>>");
 
 export const db = drizzle({ client: pool });
-
-await db.execute(`
-  CREATE TABLE IF NOT EXISTS food (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price INTEGER NOT NULL, -- stored as cents
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
-`);
-
-console.log("done");
