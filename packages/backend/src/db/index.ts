fada14169e45811cff89ba31f8b1fbcc0f7c5b72
amackzie@gmail.com <amackzie@gmail.com>
2025-06-09 18:07:03 +0300

    remove console logs - fixed issue
    issue caused by docker volume caches :(

--- packages/backend/src/db/index.ts
+++ packages/backend/src/db/index.ts
@@ -6,9 +6,4 @@ const pool = new Pool({
   connectionString: Bun.env.DATABASE_URL,
 });
 
-console.log(">>>>>>>>>>>>");
-console.log(Bun.env.DATABASE_URL);
-console.log(Bun.env.BETTER_AUTH_URL);
-console.log(">>>>>>>>>>>>");
-
 export const db = drizzle({ client: pool });
