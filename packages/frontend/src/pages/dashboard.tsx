041d80277feda952fb02746aabb97876e91bb891
ddrew <amackzie@gmail.com>
2025-12-27 22:01:59 +0300

    attempt to send api request to server

--- packages/frontend/src/pages/dashboard.tsx
+++ packages/frontend/src/pages/dashboard.tsx
@@ -1,40 +1,43 @@
 import { useState, type FC } from 'react';
+
 import { useSession } from '@/src/lib/auth';
 import { useWebSocket } from '@/src/lib/useWebSocket';
-import { useLocalStorage } from '@/src/lib/useLocalStorage';
 import { AISearchBar } from '@/src/components/search';
 
 export const Dashboard: FC = () => {
-  const [isLoading] = useState(false);
-  const [doneGenerating, setDoneGenerating] = useState<boolean>(false);
-  const [generatedWorkout, setGeneratedWorkout] =
-    useLocalStorage('generatedWorkout');
+  const [isLoading, setIsLoading] = useState(false);
   const { data: session } = useSession();
   const userid = session?.user.id;
 
-  const markCompleted = () => {
-    setGeneratedWorkout(true);
-    setDoneGenerating(true);
-  };
+  const markCompleted = () => console.log('');
 
   useWebSocket(userid, markCompleted);
 
-  const search = (_input: string) => console.log('you tried to search');
+  const search = async (_input: string) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('/api/ai-chat', {
+        method: 'POST',
+        body: JSON.stringify(_input),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        setIsLoading(false);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error); //TODO: show error in more meaningful way
+    }
+  };
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-1 flex flex-col justify-center items-center w-full">
-        {!generatedWorkout && !doneGenerating && (
-          <div className="flex flex-row items-center gap-2">
-            <div className="inline-grid *:[grid-area:1/1]">
-              <div className="status status-primary animate-ping"></div>
-              <div className="status status-primary"></div>
-            </div>
-            Configuring your profile and generating some workouts for you.
-          </div>
-        )}
-      </div>
-      <div className="p-6 pb-8">
+      <div className="flex-1 flex flex-col justify-center items-center w-full"></div>
+      <div className="p-6 pb-16">
         <AISearchBar isLoading={isLoading} onSearch={() => search('')} />
       </div>
     </div>
