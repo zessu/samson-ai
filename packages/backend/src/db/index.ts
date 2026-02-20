dd2777e30d7018ee18db8e6ffdb0fcf39b573b01
ddrew <amackzie@gmail.com>
2026-02-20 22:55:29 +0300

    add schema to types

--- packages/backend/src/db/index.ts
+++ packages/backend/src/db/index.ts
@@ -1,9 +1,11 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+import * as schema from '@db/schema/index';
 
 const pool = new Pool({
   connectionString: Bun.env.DATABASE_URL,
 });
 
-export const db = drizzle({ client: pool });
+export const db = drizzle({ client: pool, schema });
