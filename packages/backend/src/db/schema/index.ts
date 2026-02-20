c2589fa3787f6a933247c49ed032681b15470480
ddrew <amackzie@gmail.com>
2026-02-20 17:59:49 +0300

    make user unique for workout settings

--- packages/backend/src/db/schema/index.ts
+++ packages/backend/src/db/schema/index.ts
@@ -5,45 +5,46 @@ import {
   pgEnum,
   text,
   time,
-} from "drizzle-orm/pg-core";
-import { createInsertSchema, createUpdateSchema } from "drizzle-zod";
-import { user } from "../../../auth-schema";
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema, createUpdateSchema } from 'drizzle-zod';
+import { user } from '../../../auth-schema';
 
-const weekdays = pgEnum("weekdays", [
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-  "sunday",
+const weekdays = pgEnum('weekdays', [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
 ] as const);
 
-export const workoutSettings = pgTable("workoutSettings", {
-  id: text("id").primaryKey(),
+export const workoutSettings = pgTable('workoutSettings', {
+  id: text('id').primaryKey(),
   weekdays: weekdays().array(),
-  workoutTime: time("workout_time"),
-  workoutDuration: smallint("workout_duration"),
-  userTimezoneOffset: smallint("offset"),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
-  userId: text("user_id")
+  workoutTime: time('workout_time'),
+  workoutDuration: smallint('workout_duration'),
+  userTimezoneOffset: smallint('offset'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  userId: text('user_id')
     .notNull()
-    .references(() => user.id, { onDelete: "cascade" }),
+    .references(() => user.id, { onDelete: 'cascade' })
+    .unique(),
 });
 
-export const workoutSchedule = pgTable("workout_schedule", {
-  id: text("id").primaryKey(),
-  week: text("week").notNull(),
-  day: text("day").notNull(),
-  workout: text("workout").notNull(),
-  calories: smallint("calories").notNull(),
-  caution: text("caution").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
-  userId: text("user_id")
+export const workoutSchedule = pgTable('workout_schedule', {
+  id: text('id').primaryKey(),
+  week: text('week').notNull(),
+  day: text('day').notNull(),
+  workout: text('workout').notNull(),
+  calories: smallint('calories').notNull(),
+  caution: text('caution').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  userId: text('user_id')
     .notNull()
-    .references(() => user.id, { onDelete: "cascade" }),
+    .references(() => user.id, { onDelete: 'cascade' }),
 });
 
 export const workoutSettingsInsertSchema = createInsertSchema(workoutSettings);
