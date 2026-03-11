14db230e400950cb4f3cf0088cedd7bf71e9655c
ddrew <amackzie@gmail.com>
2026-03-11 20:24:46 +0300

    mod age schema

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,28 +1,31 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const onBoardingSchema = z.object({
-  gender: z.enum(["male", "female"]),
-  age: z.number(),
+  gender: z.enum(['male', 'female']),
+  age: z
+    .number()
+    .min(10, 'You must be at least 10')
+    .max(100, 'Age exceeded max'),
   weight: z.number(),
-  fitnessLevel: z.enum(["beginner", "intermediate", "advanced"]),
+  fitnessLevel: z.enum(['beginner', 'intermediate', 'advanced']),
   goals: z.array(z.string()),
   equipment: z.array(z.string()),
   weekdays: z.array(
     z.enum([
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-      "sunday",
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thursday',
+      'friday',
+      'saturday',
+      'sunday',
     ])
   ),
   time: z.string(),
   offset: z
     .number()
-    .min(-12, "Incorrect TimeZone offset")
-    .max(12, "Incorrect Timezone offset"),
+    .min(-12, 'Incorrect TimeZone offset')
+    .max(12, 'Incorrect Timezone offset'),
   duration: z.number(),
   notifications: z.object({
     email: z.boolean(),
